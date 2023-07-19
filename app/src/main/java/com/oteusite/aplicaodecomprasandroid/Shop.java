857b1744aa354cde22b4de770377fccb2114a6b1
NunoTorradas <torradasnuno@hotmail.com>
2023-07-19 02:23:27 +0100

    2

---
+++ app/src/main/java/com/oteusite/aplicaodecomprasandroid/Shop.java
@@ -0,0 +1,15 @@
+package com.oteusite.aplicaodecomprasandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+
+    }
+}
