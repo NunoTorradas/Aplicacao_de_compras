f62f426cf6fe53e924109e04ec0d2b1e62b036fd
NunoTorradas <torradasnuno@hotmail.com>
2023-07-19 14:44:32 +0100

    3

--- app/src/main/java/com/oteusite/aplicaodecomprasandroid/MainActivity.java
+++ app/src/main/java/com/oteusite/aplicaodecomprasandroid/MainActivity.java
@@ -1,14 +1,46 @@
 package com.oteusite.aplicaodecomprasandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import com.oteusite.aplicaodecomprasandroid.adapter.DiscountedProductAdapter;
+import com.oteusite.aplicaodecomprasandroid.model.DiscountedProducts;
+import com.oteusite.aplicaodecomprasandroid.R.drawable.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView discountRecyclerView;
+    DiscountedProductAdapter discountedProductAdapter;
+    List<DiscountedProducts> discountedProductsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        discountRecyclerView = findViewById(R.id.discountedRecycler);
+
+        discountedProductsList = new ArrayList<>();
+        discountedProductsList.add(new DiscountedProducts(1, R.drawable.discountberry));
+        discountedProductsList.add(new DiscountedProducts(2, R.drawable.discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(3, R.drawable.discountmeat));
+        discountedProductsList.add(new DiscountedProducts(4, R.drawable.discountberry));
+        discountedProductsList.add(new DiscountedProducts(5, R.drawable.discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(6, R.drawable.discountmeat));
+
+
+        setDiscountedRecycler(discountedProductsList);
+
+    }
+
+    private void setDiscountedRecycler(List<DiscountedProducts> dataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        discountRecyclerView.setLayoutManager(layoutManager);
+        discountedProductAdapter = new DiscountedProductAdapter(this,dataList);
+        discountRecyclerView.setAdapter(discountedProductAdapter);
     }
 }
